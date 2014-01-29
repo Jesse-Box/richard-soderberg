399108d62d63776b46583e946d2f11b98b8af73d
Jesse Box <me@jessebox.net>
2014-01-29 13:13:33 +0100

    Navigation Sorted

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
       <ul>
         <li><a href="#">home</a></li>
         <li><a href="#">about</a></li>
-        <li><a href="#">projects</a></li>
+        <li><a href="#">work</a></li>
         <li><a href="#">contact</a></li>
       </ul>
     </nav>
