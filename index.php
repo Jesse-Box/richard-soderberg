1f659d60a3e3833970e2007825710e3515584e33
Jesse Box <me@jessebox.net>
2014-01-29 13:37:49 +0100

    FitText.js
    Implemented FitText.js into my headlines
    https://github.com/davatron5000/FitText.js

--- index.php
+++ index.php
@@ -39,14 +39,18 @@
         <li><a href="#">contact</a></li>
       </ul>
     </nav>
-    <section class="landing-title">
-      <h1>RICHARD SODERBERG</h1>
+    <section>
+      <h1 id="responsive_headline">RICHARD SÖDERBERG</h1>
     </section>
   </section>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
   <script>var nav = responsiveNav(".nav-collapse");</script>
+  <script src="js/jquery.fittext.js"></script>
+  <script>
+    jQuery("#responsive_headline").fitText();
+  </script>
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
 </body>
