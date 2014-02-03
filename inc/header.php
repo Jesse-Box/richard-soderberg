9f7d814b86e261637421337cb513d298dc9a6c45
Jesse Box <me@jessebox.net>
2014-02-03 12:59:45 +0100

    Added Flexbox
    This is a experiment to implement basic Flexbox into my site

--- inc/header.php
+++ inc/header.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php echo "RICHARD SÖDERBERG"; ?></title>
+  <title><?php echo $PageTitle; ?></title>
   <meta name="description" content="Bio goes here">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php include('inc/header.php'); ?>
+<?php 
+$PageTitle = "RICHARD SÖDERBERG";
+include('inc/header.php'); ?>
 
   <section class="headline">
-      <h1 id="responsive_headline">RICHARD SÖDERBERG</h1>
-    </section>
+    <h1 id="responsive_headline">RICHARD SÖDERBERG</h1>
   </section>
 
 <?php include('inc/footer.php'); ?>
