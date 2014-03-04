66978a7fedbea903ba2457f058d23b72989d9b9e
Jesse Box <me@jessebox.net>
2014-03-04 12:35:54 +0100

    Greyscale Transitions
    Added greyscale to colour transitions to the images on the work page as
    a hover mechanic. I’m not 100% satisfied yet, I would like to repeat
    the same overall effect for the corresponding typography.

--- work.php
+++ work.php
@@ -4,28 +4,45 @@ include('inc/header.php'); ?>
 
 <section id="wrapper">
   <a href="#" class="item">
-    <img src="img/article-mock.jpg">
+    <img src="img/work/work-01.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/hero-image2.jpg">
+    <img src="img/work/work-02.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/article-mock.jpg">
+    <img src="img/work/work-03.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/hero-image2.jpg">
+    <img src="img/work/work-04.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/article-mock.jpg">
+    <img src="img/work/work-05.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/hero-image2.jpg">
+    <img src="img/work/work-06.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/article-mock.jpg">
+    <img src="img/work/work-07.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
   </a>
   <a href="#" class="item">
-    <img src="img/hero-image2.jpg">
-  </a>      
+    <img src="img/work/work-08.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
+  </a>
+  <a href="#" class="item">
+    <img src="img/work/work-09.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
+  </a>
+  <a href="#" class="item">
+    <img src="img/work/work-10.jpg" id="grayscale">
+    <h4>OBSCUR A/W 2014</h4>
+  </a>          
 </section>
+
 <?php include('inc/footer.php'); ?>
